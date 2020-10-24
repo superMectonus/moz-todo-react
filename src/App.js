3b5b14f4530eca03d6404de502b8b653f5dc98ba
superMectonus <camembertrosepoilu@gmail.com>
2020-10-24 21:51:21 +0200

    Putting it all together: Adding a task

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from "react";
+import { nanoid } from "nanoid";
 import Todo from './components/Todo';
 import Form from './components/Form';
 import FilterButton from './components/FilterButton';
@@ -6,10 +7,13 @@ import FilterButton from './components/FilterButton';
 function App(props) {
 
   function addTask(name) {
-    alert(name);
+    const newTask = { id: 'todo-'+nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
   }
 
-  const taskList = props.tasks.map(task => (
+  const [tasks, setTasks] = useState(props.tasks);
+
+  const taskList = tasks.map(task => (
     <Todo  
       id={task.id} 
       name={task.name} 
@@ -17,34 +21,25 @@ function App(props) {
       key={task.id}
     />
   ));
-  
+
   return (
     <div className="todoapp stack-large">
-      
       <h1>TodoMatic</h1>
-
       <Form addTask={addTask} />
-
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <FilterButton />
         <FilterButton />
       </div>
-
       <h2 id="list-heading">
         3 tasks remaining
       </h2>
-
-      <ul
-        // role="list"
-        className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading"
-      >
+      <ul className="todo-list stack-large stack-exception" aria-labelledby="list-heading">
         {taskList}
       </ul>
-
     </div>
   );
+
 }
 
 export default App;
