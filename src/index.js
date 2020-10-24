80d032baf2974aff8b708a1385b15d09d31bc02e
superMectonus <camembertrosepoilu@gmail.com>
2020-10-24 15:52:38 +0200

    Componentizing our React app

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App subject="Bobby" />, document.getElementById('root'));
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false }
+]
+
+ReactDOM.render(<App tasks={DATA} />, document.getElementById('root'));
