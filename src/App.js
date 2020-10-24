c1b4b9d02a1ff7fdffaf6045ce3292ce10fc71f3
superMectonus <camembertrosepoilu@gmail.com>
2020-10-24 21:11:24 +0200

    Handling events

--- src/App.js
+++ src/App.js
@@ -4,33 +4,45 @@ import Form from './components/Form';
 import FilterButton from './components/FilterButton';
 
 function App(props) {
-    const taskList = props.tasks.map(task => (
-      <Todo  
-        id={task.id} 
-        name={task.name} 
-        completed={task.completed}
-        key={task.id} 
-      />
-    ));
-    return (
+
+  function addTask(name) {
+    alert(name);
+  }
+
+  const taskList = props.tasks.map(task => (
+    <Todo  
+      id={task.id} 
+      name={task.name} 
+      completed={task.completed}
+      key={task.id}
+    />
+  ));
+  
+  return (
     <div className="todoapp stack-large">
+      
       <h1>TodoMatic</h1>
-      <Form/>
+
+      <Form addTask={addTask} />
+
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <FilterButton />
         <FilterButton />
       </div>
+
       <h2 id="list-heading">
         3 tasks remaining
       </h2>
+
       <ul
-        role="list"
+        // role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
         {taskList}
       </ul>
+
     </div>
   );
 }
